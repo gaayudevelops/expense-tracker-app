a52f1561c08fbf3ac6a83926ba79f7767a56b91c
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-08-23 11:02:52 +0530

    Task 10: Premium Account Features

--- Frontend/ExpenseTracker/expense.js
+++ Frontend/ExpenseTracker/expense.js
@@ -25,8 +25,66 @@ async function SaveToBackend(event) {
     }
 }
 
+function showpremiumusermessage(){
+    document.getElementById('rzp-btn').style.visibility = "hidden";
+    document.getElementById('prm-msg').innerHTML = "Premium User";
+    // document.getElementById('prm-msg').style.color= "blue";
+    // document.getElementById('prm-msg').style.fontSize = "20px";
+    // document.getElementById('prm-msg').style.margin = " 2rem 0 0 112rem";
+}
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+function showLeaderboard(){
+
+    const input = document.getElementById('leader');
+    const inputElement = document.createElement("input");
+    inputElement.type = 'button';
+    inputElement.value = 'Show Leaderboard';
+    inputElement.id = "showleaderboard"
+    inputElement.onclick = async() => {
+        try{
+            document.getElementById('showleaderboard').style.visibility = "hidden";
+            const token = localStorage.getItem('token');
+            const userLeaderBoardArray = await axios. get("http://localhost:3000/premium/showleaderboard",{ headers:{"Authorization":token} })
+            console.log(userLeaderBoardArray);
+            
+            const leaderboardElem = document.getElementById('ranks');
+            leaderboardElem.innerHTML += '<h1> Leader Board </h1>'
+
+            userLeaderBoardArray.data.forEach ((userDetails) => {
+            leaderboardElem.innerHTML += `<li>Name : ${userDetails.name} Total Expense : ${userDetails.total_cost}`;
+            })
+        }catch(err){
+           console.log(err);
+        }
+    }
+    input.appendChild(inputElement);
+    console.log(input)
+
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
     const token=localStorage.getItem('token'); 
+    const decodedtoken = parseJwt(token);
+    // console.log(decodedtoken);
+    const ispremiumuser = decodedtoken.ispremiumuser;
+    // console.log(ispremiumuser);
+
+    if(ispremiumuser){
+        showLeaderboard();
+        showpremiumusermessage();
+        
+    }
     axios.get("http://localhost:3000/expense/get-expenses",{
         headers:{"Authorization":token} //setting the authorisation in response headers as the tokrn encrypted
     })
@@ -67,6 +125,8 @@ function showExpenseOnScreen(obj) {
 
 }
 
+
+
 document.getElementById('rzp-btn').onclick = async function(event) {
 
     const token=localStorage.getItem('token');
@@ -75,12 +135,20 @@ document.getElementById('rzp-btn').onclick = async function(event) {
         "key": response.data.key_id,
         "order_id":response.data.order.id,
         "handler":async function (response){
-            await axios.post("http://localhost:3000/purchase/updatetransactionstatus",{
+            const res = await axios.post("http://localhost:3000/purchase/updatetransactionstatus",{
                 order_id:options.order_id,
                 payment_id:response.razorpay_payment_id
             }, {headers:{"Authorization":token}});
-            
-           alert("You are a Premuim user now!")   
+
+      
+           alert("You are a Premuim user now!");
+           localStorage.setItem('token', res.data.token);
+           showpremiumusermessage();
+           showLeaderboard();
+          
+           
+
+
         }
     };
     const rzp1 = new Razorpay(options);
